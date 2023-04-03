ad786801fa9a797d630e75d1263ca4d005fdffd5
Anand Sangabattuni <anandu396@gmail.com>
2023-04-03 16:34:51 +0530

    files added via drag and drop
    because I haven't learnt how to add files via git yet

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('signup-form');
+const errorMessage = document.getElementById('error-message');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault(); // prevent form from submitting
+
+	const name = document.getElementById('name').value.trim();
+	const email = document.getElementById('email').value.trim();
+	const password = document.getElementById('password').value;
+
+	if (name === '' || email === '' || password === '') {
+		errorMessage.innerText = 'Please fill in all fields';
+	} else if (password.length < 6) {
+		errorMessage.innerText = 'Password must be at least 6 characters long';
+	} else {
+		// submit form data to server here
+		console.log('Form submitted successfully');
+	}
+});
